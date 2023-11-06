369750351a82e74cc3713215fed57252a8afe769
michelle <junemichelle20@gmail.com>
2023-11-06 08:48:11 +0300

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import GifListContainer from './GifListContainer';
 import NavBar from "./NavBar";
 
 // the App component should render out the GifListContainer component
@@ -8,6 +8,13 @@ function App() {
   return (
     <div>
       <NavBar color="black" title="Giphy Search" />
+      <div className="container " >
+        <div className="col-md-3">
+          <div className="row">
+            <GifListContainer />
+          </div>  
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/GifList.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const GifList = (props) => {
+  const { gifs } = props;
+
+  return (
+    <ul>
+      {gifs.map((gif) => (
+        <li key={gif.id}>
+          <img src={gif.images.original.url} alt={gif.title} />
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default GifList;
